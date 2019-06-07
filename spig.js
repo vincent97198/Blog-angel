ab13ec124adb82866f247917d97b9d380891e9a1
vincent97198 <31513765+vincent97198@users.noreply.github.com>
2019-06-07 20:03:01 +0800

    Update spig.js

--- spig.js
+++ spig.js
@@ -128,16 +128,12 @@ jQuery(document).ready(function ($) {
     $(".mumu").click(function () {
         if (!ismove) {
             stat_click++;
-            if (stat_click > 4) {
-                msgs = ["你有完沒完呀？", "你已經摸我" + stat_click + "次了", "非禮呀！救命！"];
+	   if (stat_click<20){
+                msgs = [ "不要摸我了，小心我咬你！" ,"那裡不行>w<","再摸我就要報警了","你有完沒完呀？", "你已經摸我" + stat_click + "次了", "非禮呀！救命！"];
                 var i = Math.floor(Math.random() * msgs.length);
                 showMessage(msgs[i]);
-            } else if (stat_click<20){
-                msgs = [ "不要摸我了，小心我咬你！" ,"那裡不行>w<","再摸我就要報警了"];
-                var i = Math.floor(Math.random() * msgs.length);
-                showMessage(msgs[i]);
-            } else {
-		while(1){
+            } else if(stat_click>=20){
+		 while(1){
 		  var x=Math.floor(Math.random() * s.length);
 		  var y = Math.floor(Math.random() * s.length);
 		  $(".spig").css({
