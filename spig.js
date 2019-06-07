92ea1972b166135c32ac908ba026ca732aae4fb6
vincent97198 <31513765+vincent97198@users.noreply.github.com>
2019-06-07 20:34:05 +0800

    Update spig.js

--- spig.js
+++ spig.js
@@ -74,16 +74,15 @@ jQuery(document).ready(function ($) {
 jQuery(document).ready(function ($) {
 
     window.setInterval(function () {
-	    if(!lost){
-		    msgs = [ "陪我聊天吧！", "好無聊哦，你都不陪我玩！", "…@……!………", "^%#&*!@*(&#)(!)(", "我可愛吧！嘻嘻!~^_^!~~","從前有座山，山上有座廟，廟裏有個老和尚給小和尚講故事，講：“從前有座……”"];
+	if(!lost){
+	    msgs = [ "陪我聊天吧！", "好無聊哦，你都不陪我玩！", "…@……!………", "^%#&*!@*(&#)(!)(", "我可愛吧！嘻嘻!~^_^!~~","從前有座山，山上有座廟，廟裏有個老和尚給小和尚講故事，講：“從前有座……”"];
        // msgs = ["<iframe src=\"http://api.lwl12.com/hitokoto\"frameborder=\"0\" scrolling=\"no\" id=\"test\" onload=\"this.height=50\"  width=\"130px\" allowtransparency=\"true\" ></iframe>"];
         //if(weather.state)msgs.push(weather.c[0],weather.c[2]);
         //msgs = [weather.c[0],weather.c[2],"<iframe src=\"http://api.myhloli.com/hitokoto/\" frameborder=\"0\" scrolling=\"no\" id=\"external-frame\"  height=\"70px\" width=\"150px\" allowtransparency=\"true\" ></iframe>","<iframe src=\"http://api.myhloli.com/hitokoto/\" frameborder=\"0\" scrolling=\"no\" id=\"external-frame\"  height=\"70px\" width=\"150px\" allowtransparency=\"true\" ></iframe>","<iframe src=\"http://api.myhloli.com/hitokoto/\" frameborder=\"0\" scrolling=\"no\" id=\"external-frame\"  height=\"70px\" width=\"150px\" allowtransparency=\"true\" ></iframe>"];
         //msgs = [$("#hitokoto").text()];
-        var i = Math.floor(Math.random() * msgs.length);
-        showMessage(msgs[i], 8000);
-	    }
-       
+           var i = Math.floor(Math.random() * msgs.length);
+           showMessage(msgs[i], 8000);
+	}
     }, 15000);
 });
 /*
@@ -139,8 +138,8 @@ jQuery(document).ready(function ($) {
             } else { 
 		showMessage("不理你啦",1000);
 		$(".mumu").fadeTo("300", 0);
-		    
-		   delay(5000)
+		lost=true;
+		delay(5000)
             }
         s = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6,0.7,0.75,-0.1, -0.2, -0.3, -0.4, -0.5, -0.6,-0.7,-0.75];
         var i1 = Math.floor(Math.random() * s.length);
