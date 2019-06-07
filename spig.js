2c9c3a0a645504ada34294c7cb85def6788f1f22
vincent97198 <31513765+vincent97198@users.noreply.github.com>
2019-06-07 15:18:20 +0800

    Update spig.js

--- spig.js
+++ spig.js
@@ -1,16 +1,17 @@
-//右键菜单
+
+//å³é”®èœå•
 jQuery(document).ready(function ($) {
     $("#spig").mousedown(function (e) {
         if(e.which==3){
-        showMessage("秘密通道:<br />    <img src=\"http://pic.sc.chinaz.com/Files/pic/Listfaces/3646/02.gif\"/><br/><a href=\"http://cnblogs.com/zwfymqz\" title=\"自为风月马前卒\">自为风月马前卒</a>  ",10000);
-}
-});
-$("#spig").bind("contextmenu", function(e) {
-    return false;
-});
+        	showMessage("ç§˜å¯†é€šé“:<br />    <img src=\"http://pic.sc.chinaz.com/Files/pic/Listfaces/3646/02.gif\"/><br/><a href=\"http://cnblogs.com/flipped\" title=\"æ°´éƒ\">æ°´éƒ</a>  ",10000);
+		}
+	});
+	$("#spig").bind("contextmenu", function(e) {
+	    return false;
+	});
 });
 
-//鼠标在消息上时
+//é¼ æ ‡åœ¨æ¶ˆæ¯ä¸Šæ—¶
 jQuery(document).ready(function ($) {
     $("#message").hover(function () {
        $("#message").fadeTo("100", 1);
@@ -18,12 +19,11 @@ jQuery(document).ready(function ($) {
 });
 
 
-//鼠标在上方时
+//é¼ æ ‡åœ¨ä¸Šæ–¹æ—¶
 jQuery(document).ready(function ($) {
-    //$(".mumu").jrumble({rangeX: 2,rangeY: 2,rangeRot: 1});
     $(".mumu").mouseover(function () {
        $(".mumu").fadeTo("300", 0.3);
-       msgs = ["我隐身了，你看不到我", "我会隐身哦！嘿嘿！", "别动手动脚的，把手拿开！", "把手拿开我才出来！"];
+       msgs = ["æˆ‘éšèº«äº†ï¼Œä½ çœ‹ä¸åˆ°æˆ‘", "æˆ‘ä¼šéšèº«å“¦ï¼å˜¿å˜¿ï¼", "åˆ«åŠ¨æ‰‹åŠ¨è„šçš„ï¼ŒæŠŠæ‰‹æ‹¿å¼€ï¼", "æŠŠæ‰‹æ‹¿å¼€æˆ‘æ‰å‡ºæ¥ï¼"];
        var i = Math.floor(Math.random() * msgs.length);
         showMessage(msgs[i]);
     });
@@ -33,24 +33,26 @@ jQuery(document).ready(function ($) {
 });
 
 
-//开始
+//å¼€å§‹
 jQuery(document).ready(function ($) {
-    if (isindex) { //如果是主页
+	var url = window.location.href;
+	var title = document.title;
+    if (url.indexOf('/p/') < 0) { //å¦‚æžœæ˜¯ä¸»é¡µ
         var now = (new Date()).getHours();
         if (now > 0 && now <= 6) {
-            showMessage(visitor + ' 你是夜猫子呀？还不睡觉，明天起的来么你？', 6000);
+            showMessage(visitor + ' ä½ æ˜¯å¤œçŒ«å­å‘€ï¼Ÿè¿˜ä¸ç¡è§‰ï¼Œæ˜Žå¤©èµ·çš„æ¥ä¹ˆä½ ï¼Ÿ', 6000);
         } else if (now > 6 && now <= 11) {
-            showMessage(visitor + ' 早上好，早起的鸟儿有虫吃噢！早起的虫儿被鸟吃，你是鸟儿还是虫儿？嘻嘻！', 6000);
+            showMessage(visitor + ' æ—©ä¸Šå¥½ï¼Œæ—©èµ·çš„é¸Ÿå„¿æœ‰è™«åƒå™¢ï¼æ—©èµ·çš„è™«å„¿è¢«é¸Ÿåƒï¼Œä½ æ˜¯é¸Ÿå„¿è¿˜æ˜¯è™«å„¿ï¼Ÿå˜»å˜»ï¼', 6000);
         } else if (now > 11 && now <= 14) {
-            showMessage(visitor + ' 中午了，吃饭了么？不要饿着了，饿死了谁来挺我呀！', 6000);
+            showMessage(visitor + ' ä¸­åˆäº†ï¼Œåƒé¥­äº†ä¹ˆï¼Ÿä¸è¦é¥¿ç€äº†ï¼Œé¥¿æ­»äº†è°æ¥æŒºæˆ‘å‘€ï¼', 6000);
         } else if (now > 14 && now <= 18) {
-            showMessage(visitor + ' 中午的时光真难熬！还好有你在！', 6000);
+            showMessage(visitor + ' ä¸­åˆçš„æ—¶å…‰çœŸéš¾ç†¬ï¼è¿˜å¥½æœ‰ä½ åœ¨ï¼', 6000);
         } else {
-            showMessage(visitor + ' 快来逗我玩吧！', 6000);
+            showMessage(visitor + ' å¿«æ¥é€—æˆ‘çŽ©å§ï¼', 6000);
         }
     }
     else {
-        showMessage('欢迎' + visitor + '来到《' + title + '》', 6000);
+        showMessage('æ¬¢è¿Ž' + visitor + 'æ¥åˆ°ã€Š' + title + 'ã€‹', 6000);
     }
     $(".spig").animate({
         top: $(".spig").offset().top + 300,
@@ -60,80 +62,75 @@ jQuery(document).ready(function ($) {
         queue: false,
         duration: 1000
     });
+//    window.setTimeout(function () {
+//        showMessage("ä¸‹é¢æ’­æŠ¥æ˜Žæ—¥å¤©æ°”<iframe name=\"xidie\" src=\"http://api.lwl12.com/hitokoto/?encode=js\"frameborder=\â€œ0\â€ scrolling=\"no\"  width=\"130px\" allowtransparency=\"true\" ></iframe>", 10000);
+//    },
+//  4000);
 });
 
-//鼠标在某些元素上方时
+//é¼ æ ‡åœ¨æŸäº›å…ƒç´ ä¸Šæ–¹æ—¶
 jQuery(document).ready(function ($) {
-    $('h2 a').click(function () {//标题被点击时
-        showMessage('萌萌地加载《<span style="color:#0099cc;">' + $(this).text() + '</span>》中，请稍候');
+    $('h2 a').click(function () {//æ ‡é¢˜è¢«ç‚¹å‡»æ—¶
+        showMessage('èŒèŒåœ°åŠ è½½ã€Š<span style="color:#0099cc;">' + $(this).text() + '</span>ã€‹ä¸­ï¼Œè¯·ç¨å€™');
     });
     $('h2 a').mouseover(function () {
-        showMessage('要看看《<span style="color:#0099cc;">' + $(this).text() + '</span>》这篇随笔么？');
+        showMessage('è¦çœ‹çœ‹ã€Š<span style="color:#0099cc;">' + $(this).text() + '</span>ã€‹è¿™ç¯‡éšç¬”ä¹ˆï¼Ÿ');
     });
-    $('#prev-page').mouseover(function(){
-        showMessage('要翻到上一页吗?');
+    $('li a').mouseover(function () {
+        showMessage('åŽ» <span style="color:#0099cc;">' + $(this).text() + '</span> é€›é€›');
     });
-    $('#next-page').mouseover(function(){
-        showMessage('要翻到下一页吗?');
+    $('.tbCommentBody').mouseover(function () {
+        showMessage('<span style="color:#0099cc;">' + visitor + '</span> å‘è¯„è®ºæ å‡ºå‘å§ï¼');
     });
-    $('#index-links li a').mouseover(function () {
-        showMessage('去 <span style="color:#0099cc;">' + $(this).text() + '</span> 逛逛');
+    $('#btn_comment_submit').mouseover(function () {
+        showMessage('ç¡®è®¤æäº¤äº†ä¹ˆï¼Ÿ');
     });
-    $('.tbCommentBodys').mouseover(function () {
-        showMessage('<span style="color:#0099cc;">' + visitor + '</span> 向评论栏出发吧！');
-    });
-    $('#submit').mouseover(function () {
-        showMessage('确认提交了么？');
-    });
-    $('#s').focus(function () {
-        showMessage('输入你想搜索的关键词再按Enter(回车)键就可以搜索啦!');
-    });
-    $('#go-prev').mouseover(function () {
-        showMessage('点它可以后退哦！');
-    });
-    $('#go-next').mouseover(function () {
-        showMessage('点它可以前进哦！');
-    });
-    $('#refresh').mouseover(function () {
-        showMessage('点它可以重新载入此页哦！');
-    });
-    $('#go-home').mouseover(function () {
-        showMessage('点它就可以回到首页啦！');
-    });
-    $('#addfav').mouseover(function () {
-        showMessage('点它可以把此页加入书签哦！');
-    });
-    $('#nav-two a').mouseover(function () {
-        showMessage('嘘，从这里可以进入控制面板的哦！');
-    });
-    $('.post-category a').mouseover(function () {
-        showMessage('点击查看此分类下得所有文章');
-    });
-    $('.post-heat a').mouseover(function () {
-        showMessage('点它可以直接跳到评论列表处.');
-    });
-    $('#tho-shareto span a').mouseover(function () {
-        showMessage('你知道吗?点它可以分享本文到'+$(this).attr('title'));
-    });
-    $('#switch-to-wap').mouseover(function(){
-        showMessage('点击可以切换到手机版博客版面');
+    $('#q').focus(function () {
+        showMessage('è¾“å…¥ä½ æƒ³æœç´¢çš„å…³é”®è¯å†æŒ‰Enter(å›žè½¦)é”®å°±å¯ä»¥æœç´¢å•¦!');
     });
 });
 
 
-//无聊讲点什么
+//æ— èŠè®²ç‚¹ä»€ä¹ˆ
 jQuery(document).ready(function ($) {
 
     window.setInterval(function () {
-        msgs = [ "陪我聊天吧！", "好无聊哦，你都不陪我玩！", "…@……!………", "^%#&*!@*(&#)(!)(", "我可爱吧！嘻嘻!~^_^!~~","谁淫荡呀?~谁淫荡?，你淫荡呀!~~你淫荡！~~","从前有座山，山上有座庙，庙里有个老和尚给小和尚讲故事，讲：“从前有座……”"];
+       // msgs = [ "é™ªæˆ‘èŠå¤©å§ï¼", "å¥½æ— èŠå“¦ï¼Œä½ éƒ½ä¸é™ªæˆ‘çŽ©ï¼", "â€¦@â€¦â€¦!â€¦â€¦â€¦", "^%#&*!@*(&#)(!)(", "æˆ‘å¯çˆ±å§ï¼å˜»å˜»!~^_^!~~","è°æ·«è¡å‘€?~è°æ·«è¡?ï¼Œä½ æ·«è¡å‘€!~~ä½ æ·«è¡ï¼~~","ä»Žå‰æœ‰åº§å±±ï¼Œå±±ä¸Šæœ‰åº§åº™ï¼Œåº™é‡Œæœ‰ä¸ªè€å’Œå°šç»™å°å’Œå°šè®²æ•…äº‹ï¼Œè®²ï¼šâ€œä»Žå‰æœ‰åº§â€¦â€¦â€"];
+       // msgs = ["<iframe src=\"http://api.lwl12.com/hitokoto\"frameborder=\"0\" scrolling=\"no\" id=\"test\" onload=\"this.height=50\"  width=\"130px\" allowtransparency=\"true\" ></iframe>"];
+        //if(weather.state)msgs.push(weather.c[0],weather.c[2]);
+        //msgs = [weather.c[0],weather.c[2],"<iframe src=\"http://api.myhloli.com/hitokoto/\" frameborder=\"0\" scrolling=\"no\" id=\"external-frame\"  height=\"70px\" width=\"150px\" allowtransparency=\"true\" ></iframe>","<iframe src=\"http://api.myhloli.com/hitokoto/\" frameborder=\"0\" scrolling=\"no\" id=\"external-frame\"  height=\"70px\" width=\"150px\" allowtransparency=\"true\" ></iframe>","<iframe src=\"http://api.myhloli.com/hitokoto/\" frameborder=\"0\" scrolling=\"no\" id=\"external-frame\"  height=\"70px\" width=\"150px\" allowtransparency=\"true\" ></iframe>"];
+        msgs = [$("#hitokoto").text()];
         var i = Math.floor(Math.random() * msgs.length);
         showMessage(msgs[i], 8000);
     }, 15000);
 });
-//评论资料
+/*
+//æ— èŠåŠ¨åŠ¨
+jQuery(document).ready(function ($) {
+    window.setInterval(function () {
+       // msgs = ["æ’­æŠ¥æ˜Žæ—¥å¤©æ°”<iframe name=\"xidie\" src=\"http://m.weather.com.cn/data/101010100.html\"frameborder=\â€œ0\â€ scrolling=\"no\" height=\"15px\"  width=\"130px\" allowtransparency=\"true\" ></iframe>", "ä¹¾å¤å¤§æŒªç§»ï¼", "æˆ‘é£˜è¿‡æ¥äº†ï¼~", "æˆ‘é£˜è¿‡åŽ»äº†", "æˆ‘å¾—æ„åœ°é£˜ï¼~é£˜ï¼~"];
+       // msgs = ["<iframe src=\"http://api.lwl12.com/hitokoto\"frameborder=\"0\" scrolling=\"no\" id=\"test\" onload=\"this.height=50\"  width=\"140px\" allowtransparency=\"true\" ></iframe>"];
+        // if(weather.state)msgs.push(weather.c[0],weather.c[2]);
+         msgs = [weather.c[0],weather.c[2],"<iframe src=\"http://api.myhloli.com/hitokoto/\" frameborder=\"0\" scrolling=\"no\" id=\"external-frame\"  height=\"70px\" width=\"150px\" allowtransparency=\"true\" ></iframe>","<iframe src=\"http://api.myhloli.com/hitokoto/\" frameborder=\"0\" scrolling=\"no\" id=\"external-frame\"  height=\"70px\" width=\"150px\" allowtransparency=\"true\" ></iframe>","<iframe src=\"http://api.myhloli.com/hitokoto/\" frameborder=\"0\" scrolling=\"no\" id=\"external-frame\"  height=\"70px\" width=\"150px\" allowtransparency=\"true\" ></iframe>"];
+        var i = Math.floor(Math.random() * msgs.length);
+        s = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6,0.7,0.75,-0.1, -0.2, -0.3, -0.4, -0.5, -0.6,-0.7,-0.75];
+        var i1 = Math.floor(Math.random() * s.length);
+        var i2 = Math.floor(Math.random() * s.length);
+            $(".spig").animate({
+            left: document.body.offsetWidth/2*(1+s[i1]),
+            top:  document.body.offsetheight/2*(1+s[i1])
+        },
+            {
+                duration: 2000,
+                complete: showMessage(msgs[i])
+            });
+    }, 45000);
+});
+*/
+//è¯„è®ºèµ„æ–™
 jQuery(document).ready(function ($) {
     $("#author").click(function () {
-        showMessage("留下你的尊姓大名！");
+        showMessage("ç•™ä¸‹ä½ çš„å°Šå§“å¤§åï¼");
         $(".spig").animate({
             top: $("#author").offset().top - 70,
             left: $("#author").offset().left - 170
@@ -144,7 +141,7 @@ jQuery(document).ready(function ($) {
         });
     });
     $("#email").click(function () {
-        showMessage("留下你的邮箱，不然就是无头像人士了！");
+        showMessage("ç•™ä¸‹ä½ çš„é‚®ç®±ï¼Œä¸ç„¶å°±æ˜¯æ— å¤´åƒäººå£«äº†ï¼");
         $(".spig").animate({
             top: $("#email").offset().top - 70,
             left: $("#email").offset().left - 170
@@ -156,7 +153,7 @@ jQuery(document).ready(function ($) {
     });
     $("#url").click(function () {
 
-        showMessage("快快告诉我你的家在哪里，好让我去参观参观！");
+        showMessage("å¿«å¿«å‘Šè¯‰æˆ‘ä½ çš„å®¶åœ¨å“ªé‡Œï¼Œå¥½è®©æˆ‘åŽ»å‚è§‚å‚è§‚ï¼");
         $(".spig").animate({
             top: $("#url").offset().top - 70,
             left: $("#url").offset().left - 170
@@ -166,11 +163,11 @@ jQuery(document).ready(function ($) {
             duration: 1000
         });
     });
-    $("#tbCommentBody").click(function () {
-        showMessage("认真填写哦！不然会被认作垃圾评论的！我的乖乖~");
+    $("#comment").click(function () {
+        showMessage("è®¤çœŸå¡«å†™å“¦ï¼ä¸ç„¶ä¼šè¢«è®¤ä½œåžƒåœ¾è¯„è®ºçš„ï¼æˆ‘çš„ä¹–ä¹–~");
         $(".spig").animate({
-            top: $("#tbCommentBody").offset().top - 70,
-            left: $("#tbCommentBody").offset().left - 170
+            top: $("#comment").offset().top - 70,
+            left: $("#comment").offset().left - 170
         },
         {
             queue: false,
@@ -180,7 +177,7 @@ jQuery(document).ready(function ($) {
 });
 
 var spig_top = 50;
-//滚动条移动
+//æ»šåŠ¨æ¡ç§»åŠ¨
 jQuery(document).ready(function ($) {
     var f = $(".spig").offset().top;
     $(window).scroll(function () {
@@ -194,18 +191,18 @@ jQuery(document).ready(function ($) {
     });
 });
 
-//鼠标点击时
+//é¼ æ ‡ç‚¹å‡»æ—¶
 jQuery(document).ready(function ($) {
     var stat_click = 0;
     $(".mumu").click(function () {
-        
+        if (!ismove) {
             stat_click++;
             if (stat_click > 4) {
-                msgs = ["你有完没完呀？", "你已经摸我" + stat_click + "次了", "非礼呀！救命！OH，My ladygaga"];
+                msgs = ["ä½ æœ‰å®Œæ²¡å®Œå‘€ï¼Ÿ", "ä½ å·²ç»æ‘¸æˆ‘" + stat_click + "æ¬¡äº†", "éžç¤¼å‘€ï¼æ•‘å‘½ï¼OHï¼ŒMy ladygaga"];
                 var i = Math.floor(Math.random() * msgs.length);
                 //showMessage(msgs[i]);
             } else {
-                msgs = ["筋斗云！~我飞！", "我跑呀跑呀跑！~~", "别摸我，大男人，有什么好摸的！", "惹不起你，我还躲不起你么？", "不要摸我了，我会告诉老婆来打你的！", "干嘛动我呀！小心我咬你！"];
+                msgs = ["ç­‹æ–—äº‘ï¼~æˆ‘é£žï¼", "æˆ‘è·‘å‘€è·‘å‘€è·‘ï¼~~", "åˆ«æ‘¸æˆ‘ï¼Œå¤§ç”·äººï¼Œæœ‰ä»€ä¹ˆå¥½æ‘¸çš„ï¼", "æƒ¹ä¸èµ·ä½ ï¼Œæˆ‘è¿˜èº²ä¸èµ·ä½ ä¹ˆï¼Ÿ", "ä¸è¦æ‘¸æˆ‘äº†ï¼Œæˆ‘ä¼šå‘Šè¯‰è€å©†æ¥æ‰“ä½ çš„ï¼", "å¹²å˜›åŠ¨æˆ‘å‘€ï¼å°å¿ƒæˆ‘å’¬ä½ ï¼"];
                 var i = Math.floor(Math.random() * msgs.length);
                 //showMessage(msgs[i]);
             }
@@ -220,12 +217,13 @@ jQuery(document).ready(function ($) {
                 duration: 500,
                 complete: showMessage(msgs[i])
             });
-        
+        } else {
+            ismove = false;
+        }
     });
 });
 
-
-//显示消息函数 
+//æ˜¾ç¤ºæ¶ˆæ¯å‡½æ•° 
 function showMessage(a, b) {
     if (b == null) b = 10000;
     jQuery("#message").hide().stop();
@@ -235,10 +233,10 @@ function showMessage(a, b) {
     jQuery("#message").fadeOut(b);
 };
 
-//拖动
+//æ‹–åŠ¨
 var _move = false;
-var ismove = false; //移动标记
-var _x, _y; //鼠标离控件左上角的相对位置
+var ismove = false; //ç§»åŠ¨æ ‡è®°
+var _x, _y; //é¼ æ ‡ç¦»æŽ§ä»¶å·¦ä¸Šè§’çš„ç›¸å¯¹ä½ç½®
 jQuery(document).ready(function ($) {
     $("#spig").mousedown(function (e) {
         _move = true;
@@ -255,7 +253,7 @@ jQuery(document).ready(function ($) {
                 $("#spig").css({
                     top: y,
                     left: x
-                }); //控件新位置
+                }); //æŽ§ä»¶æ–°ä½ç½®
             ismove = true;
             }
         }
